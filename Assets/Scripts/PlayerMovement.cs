7bc894e723075f741d6d8cb4396e8ef4b6fa9ff9
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2016-10-14 11:45:41 -0300

    Agregado Player y script de movimiento

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // The Player's speed
+    public float speed = 10.0f;
+    //Game boundaries
+    private float leftWall = -4f;
+    private float rightWall = 4f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Get the horizontal axis that by default is bound to the arrow keys
+        // The value is in the range -1 to 1
+        // Make it move per seconds instead of frames
+        float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        // Move along the object's x-axis within the floor bounds
+        if (transform.position.x + translation < rightWall && transform.position.x + translation > leftWall)
+            transform.Translate(translation, 0, 0);
+    }
+}
